abab9e5a360aea4a875604e6285181e7cd418479
abduyus <35887528+abduyus@users.noreply.github.com>
2023-04-02 17:02:29 +0100

    Create columns, image and description.

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg")
+
+with col2:
+    st.title("Yusuf Abdur-Rasheed")
+    content = """
+    Hello, I am Yusuf! A beginner programmer who is 12 years old. I am situated in Solihull, England and go to Solihull
+    School. I am experienced in HTML and CSS and learning python.
+    """
+    st.info(content)
