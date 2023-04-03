4dee1e045394269d245acbf527f52a215cb879a5
abduyus <35887528+abduyus@users.noreply.github.com>
2023-04-03 08:30:12 +0100

    Day 22: Render the app titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -10,11 +11,23 @@ with col1:
 with col2:
     st.title("Yusuf Abdur-Rasheed")
     content = """
-    Hello, I am Yusuf! A beginner programmer who is 12 years old. I am situated in Solihull, England and go to Solihull
-    School. I am experienced in HTML and CSS and learning python.
+    Hello, I am Yusuf! A beginner programmer who is 12 years old. I am situated in Solihull, England 
+    and go to Solihull School. I am experienced in HTML and CSS and learning python.
     """
     st.info(content)
 content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
